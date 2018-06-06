88b14efd93a2ba37acbd21c8e84d7c9262fb946e
user <user@PAVILION-10>
2018-06-06 11:24:09 +0530

    class to add employee

---
+++ EmployeeManagement/src/employeemanagement/addemp.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeemanagement;
+
+/**
+ *
+ * @author user
+ */
+class addemp {
+   
+    int age;
+    String name,address;
+    double salary;
+    
+    
+   void account()
+    {
+       this.age=0;
+       this.name=null;
+       this.address=null;
+       this.salary=0;
+    }
+
+}
