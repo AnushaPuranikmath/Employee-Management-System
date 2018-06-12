a37aa8a31f1179a6d7cdf4e29e5dc1bcfd0e4ca9
user <user@PAVILION-10>
2018-06-12 22:10:28 +0530

    Employee Management System

--- EmployeeManagement/src/employeemanagement/EmployeeManagement.java
+++ EmployeeManagement/src/employeemanagement/EmployeeManagement.java
@@ -11,77 +11,40 @@ import java.util.ArrayList;
 
 
 /**
- *
- * @author Anusha 
+ * The project, Employee Management System contains various operations like adding,delete,edit and display
+ * the employees working in a particular organization.
+ * @author Anusha
  * @author Vishwanath
  */
 
 class employee
 {
     static ArrayList<addemp> acc = new ArrayList();
-    int emp_id=0;
+    static int emp_id=0;
     Scanner input = new Scanner(System.in);
-     
+    /**
+     * The function create_emp is used to add different employees by generating an employee id
+     * for each individual employee.
+     * @param name takes the name of the employee
+     * @param age takes the age of the employee
+     * @param salary takes the salary offered to the employee
+     * @param address takes the address of the employee
+     */
     public void creat_emp(String name, int age, double salary, String address)
-    {    
-       addemp a = new addemp();
-        a.name=name;
-        a.age=age;
-        a.salary=salary;
-        a.address=address; 
+    {  
+        
+       addemp a = new addemp(); 
+       a.name=name;
+       a.age=age;
+       a.salary=salary;
+       a.address=address; 
        acc.add(a);
        System.out.println("The employee ID is\n" + emp_id);
        emp_id++; 
       
     }
-    public void delete_emp()
-    {
-        System.out.println("Enter the employee ID to be deleted:");
-        emp_id=input.nextInt();
-        acc.remove(emp_id);
-        System.out.println("Details of the employee deleted!!");
-       
-    }
-    
-    public void update_emp()
-    {
-       int temp=0;
-       System.out.println("Enter the Employee ID to be updated:");
-       temp=input.nextInt();
-       acc.remove(temp);
-       temp--;
-       System.out.println("Enter new details:\n\n");
-       System.out.println("Enter the employee name:");
-       String name=input.next();
-                
-       System.out.println("Enter age of the employee:");
-       int age=input.nextInt();
-               
-       System.out.println("Enter Salary of the employee");
-       double salary = input.nextDouble();
-              
-       System.out.println("Enter address of the emplyee");
-       String address=input.next();
-      
-       System.out.println("The new employee ID is\n" + ++temp);     
-    }
-    
-    public void display(int id)
-    {
-       
-      if(id<=emp_id)
-        {
-            System.out.println("Name:"+acc.get(id).name);
-            System.out.println("Age:"+acc.get(id).age);
-            System.out.println("Address:"+acc.get(id).address);
-            System.out.println("Salary:"+acc.get(id).salary);
-        } else 
-            System.out.println("Not found\n");
-    
-    }
     
 }
-
 
 public class EmployeeManagement {
 
@@ -90,10 +53,11 @@ public class EmployeeManagement {
      */
     
     
-    public static void main(String[] args) {
-       employee e = new employee();
+    public static void main(String[] args) 
+    {
+    employee e = new employee();
     Scanner input = new Scanner(System.in);
-    int emp_age;
+    int emp_age,emp_id;
     String emp_name,emp_address;
     double emp_salary;
     int choice;
@@ -105,7 +69,7 @@ public class EmployeeManagement {
     switch (choice)
     {
         case 1: System.out.println("Enter the employee name:");
-                 emp_name=input.next();
+                emp_name=input.next();
                 
                 System.out.println("Enter age of the employee:");
                 emp_age=input.nextInt();
@@ -114,32 +78,40 @@ public class EmployeeManagement {
                 emp_salary = input.nextDouble();
               
                 System.out.println("Enter address of the emplyee");
-                 emp_address=input.next();
-      
+                emp_address=input.next();
+            /**
+             * Takes input of the following details and and send them to the creat_emp function
+             */
                 e.creat_emp(emp_name,emp_age,emp_salary,emp_address);
             break;
         
-        case 2: e.delete_emp();
+        case 2: delete d = new delete();
+                d.delete();
             break;
             
-        case 3: e.update_emp();
+        case 3: System.out.println("Enter employee ID: ");
+                emp_id=input.nextInt();
+                update up = new update();
+                up.update(emp_id);
             break;
             
         case 4: System.out.println("Enter employee ID: ");
-                int emp_id=input.nextInt();                
-                e.display(emp_id);
+                emp_id=input.nextInt();
+                display dis = new display();
+                dis.display(emp_id);
             break;
         
-        case 5:System.exit(0);
+        case 5: System.exit(0);
             break;
             
-        default : System.out.println("Enter choice from 1-4");
+        default: System.out.println("Enter choice from 1-4");
             break;
     }
         
-    }while(choice!=5);
+   }while(choice!=5);
+      
+  }
     
-    }
 }
     
     
